b68c405f6631dbf3d0d54991668fd426488dbaf0
hosak94 <yigitcanhosak@gmail.com>
2023-06-02 02:17:29 +0300

    course work completed

---
+++ api/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const server = express();
+const router = require("./users/users-router");
+
+server.use(express.json()); //json istekleri destekler.
+
+server.use("/api", router);
+
+server.get("/", (req, res) => {
+  res.json({ message: "express is working" });
+});
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+// eslint-disable-next-line no-undef
+
+const server = require("./api/server");
+
+// eslint-disable-next-line no-undef
+const port = process.env.PORT || "9001";
+
+server.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});
